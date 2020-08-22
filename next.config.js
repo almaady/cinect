4cd9b26cfd99710489079690c3e8eb81c9883731
Adriana Lopez <adriana.lopave@gmail.com>
2020-08-21 20:27:41 -0500

    Update: Nex.config.js

--- next.config.js
+++ next.config.js
@@ -5,15 +5,14 @@ const nextConfig = {
   distDir: "build",
   publicRuntimeConfig: {
     MOVIE_API_KEY: process.env.MOVIE_API_KEY,
-    MOVIE_API_URL: process.env.MOVIE_API_URL
+    MOVIE_API_URL: process.env.MOVIE_API_URL,
   },
   compress: true,
 };
 
-
 module.exports = withPlugins(
-    [
-      withImages,
+  [
+    withImages,
     nextConfig,
-]
+  ],
 );
