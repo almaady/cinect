bcd9d9783c06767a48344ba6447bc0010ad359c6
Adriana Lopez <adriana.lopave@gmail.com>
2020-08-21 21:03:52 -0500

    Update: next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
-  distDir: "build",
+  distDir: "out",
   publicRuntimeConfig: {
     MOVIE_API_KEY: process.env.MOVIE_API_KEY,
     MOVIE_API_URL: process.env.MOVIE_API_URL,
